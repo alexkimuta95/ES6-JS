3de6152b88e6d9d81d24b65b31c97135c06750b1
Alex Kimutai <billroberts2001@gmail.com>
2019-12-27 19:51:34 +0300

    ES6 2015

---
+++ main.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector('.items');
+
+// ul.remove()
+// ul.lastElementChild.remove()
+ul.firstElementChild.textContent = 'aleki';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML= '<h1>This is it</h1>';
+
